cc60f5f10e5a7f9b9073a6933247f33541ab9923
UsielOM <90886892+UsielOM@users.noreply.github.com>
2022-03-09 12:55:11 -0600

    SECCION 1 COMPLETADA

--- GRID/JS/index.js
+++ GRID/JS/index.js
@@ -1,5 +1,5 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
+const navToggle = document.querySelector(".nav-togglei")
+const navMenu = document.querySelector(".nav-menui")
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
     if (navMenu.classList.contains("nav-menu_visible")) {
